8d150b5f959d0db9863af6cdfd0ed3df081e7131
jesusharveyB <reybernal250410@outlook.com>
2025-07-27 00:04:05 -0500

    feat: agregué cambios en NavMenu, app.css y theme.css con soporte visual mejorado

---
+++ wwwroot/Js/theme.js
@@ -0,0 +1,11 @@
+﻿window.cambiarTema = (tema) => {
+    const body = document.body;
+
+    // Limpiar clases anteriores
+    body.classList.remove("dark", "red");
+
+    // Agregar la nueva
+    body.classList.add(tema);
+
+    console.log("✅ Tema aplicado: " + tema);
+};
